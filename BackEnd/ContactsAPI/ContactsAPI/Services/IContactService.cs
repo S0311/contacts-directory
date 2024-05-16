280ea8afe8b782f20102a55beac1c64e65568139
S0311 <70951078+S0311@users.noreply.github.com>
2024-05-16 13:55:55 +0530

    Update IContactService.cs
    Inferface Changes

--- BackEnd/ContactsAPI/ContactsAPI/Services/IContactService.cs
+++ BackEnd/ContactsAPI/ContactsAPI/Services/IContactService.cs
@@ -7,5 +7,11 @@ namespace ContactsAPI.Services
 {
     public interface IContactService
     {
+         Task<IEnumerable<Contact>> GetAllContacts();
+ Task<Contact> GetContactById(int id);
+ Task<Contact> CreateContact(Contact contact);
+ Task<Contact> UpdateContact(int id, Contact contact);
+ Task<bool> DeleteContact(int id);
+        
     }
 }
